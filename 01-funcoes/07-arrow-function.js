800ed69dd2e9fcea76b92229763eb8cfe2f95b0b
PEDRO ARTHUR ALVES DA SILVA <26171629@edu119.local>
2026-08-18 15:45:08 -0300

    atualizando

--- 01-funcoes/07-arrow-function.js
+++ 01-funcoes/07-arrow-function.js
@@ -1,15 +1,14 @@
-function somarTradicional(a, b){
+function somarTradicional(a, b) {
     return a + b;
 }
 
-    const somarAnonima = function (a, b){
-        return a + b;
-    }
-     const somarArrowFuction = function (a, b) =>{
-        return a + b;
-    }
+const somarAnonima = function (a, b) {
+    return a + b;
+}
+const somarArrowFuction = (a, b) => {
+    return a + b;
+}
 
-    console.log(somarTradicional(10, 5));
-    console.log(somarAnonima(10, 5));
-    console.log(somarArrowFuction(10, 5));
-    
+console.log(somarTradicional(10, 5));
+console.log(somarAnonima(10, 5));
+console.log(somarArrowFuction(10, 5));
