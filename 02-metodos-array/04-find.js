bf4c3516d7678e427a9518cdbf1afbf103864205
PEDRO ARTHUR ALVES DA SILVA <26171629@edu119.local>
2026-08-18 14:13:06 -0300

    Atualizando

---
+++ 02-metodos-array/04-find.js
@@ -0,0 +1,8 @@
+const alunos = ["Julia", "Sofia", "Pablo", "larissa"];
+
+const aluno = alunos.find((nome) =>  nome === "Sofia" );
+
+console.log(aluno);
+
+const aluno1 = alunos.find((nome) => nome === "Isabela");
+console.log(aluno1);
---
+++ 02-metodos-array/05-find-index.js
@@ -0,0 +1,10 @@
+let nomes = ["João", "Julia", "Isabela"];
+
+const indce = nomes.findIndex((n) => n === "Larissa");
+
+console.log(indice);
+
+const indice1 = nomes.findIndex((n) => n === "Pedro");
+
+console.log(indice1);
+
